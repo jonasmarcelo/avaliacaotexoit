a2ea7db9be471ee2cc98c563f240d86856ed08f2
Jonas Marcelo <marcelo.jonas@yahoo.com.br>
2022-02-15 22:39:31 -0300

    Subindo o projeto para o GIT

---
+++ src/main/java/br/com/avaliacaotexoit/AvaliacaoTexoItApplication.java
@@ -0,0 +1,13 @@
+package br.com.avaliacaotexoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoTexoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoTexoItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/avaliacaotexoit/AvaliacaoTexoItApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avaliacaotexoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoTexoItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
